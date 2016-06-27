2824a9f1664440d98884092c96870c7d00fbddcb
insiva <matteozuo@gmail.com>
2016-06-27 11:29:57 +0800

    wakelock

--- src/com/matteo/cc/sip/manager/WakeManaer.java
+++ src/com/matteo/cc/sip/manager/WakeManaer.java
@@ -5,23 +5,32 @@ import android.content.Context;
 import android.os.PowerManager;
 
 public class WakeManaer {
+	private static KeyguardManager mKeyguardManager;
+	private static PowerManager mPowerManager;
+	private static PowerManager.WakeLock mWakeLock;
 
 	public static void wakeUp(Context context) {
-		KeyguardManager km = (KeyguardManager) context
+		mKeyguardManager = (KeyguardManager) context
 				.getSystemService(Context.KEYGUARD_SERVICE);
-		KeyguardManager.KeyguardLock kl = km.newKeyguardLock("unLock");
+		KeyguardManager.KeyguardLock kl = mKeyguardManager.newKeyguardLock("unLock");
 		// 解锁
 		kl.disableKeyguard();
 		// 获取电源管理器对象
-		PowerManager pm = (PowerManager) context
+		mPowerManager = (PowerManager) context
 				.getSystemService(Context.POWER_SERVICE);
 		// 获取PowerManager.WakeLock对象,后面的参数|表示同时传入两个值,最后的是LogCat里用的Tag
-		PowerManager.WakeLock wl = pm.newWakeLock(
+		mWakeLock = mPowerManager.newWakeLock(
 				PowerManager.ACQUIRE_CAUSES_WAKEUP
 						| PowerManager.SCREEN_DIM_WAKE_LOCK, "bright");
 		// 点亮屏幕
-		wl.acquire();
+		mWakeLock.acquire();
 		// 释放
-		wl.release();
+		//mWakeLock.release();
+	}
+	
+	public static void lock(Context context){
+		if(mWakeLock!=null){
+			//mWakeLock.release();
+		}
 	}
 }
--- src/com/matteo/cc/sip/ui/activity/CallActivity.java
+++ src/com/matteo/cc/sip/ui/activity/CallActivity.java
@@ -1,6 +1,5 @@
 package com.matteo.cc.sip.ui.activity;
 
-
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -13,6 +12,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -34,16 +37,16 @@ import com.matteo.cc.sip.ui.view.FunctionsView.FunctionsListener;
 import com.matteo.cc.sip.utils.ContactUtils;
 
 public class CallActivity extends Activity implements FunctionsListener,
-		CallControlListener {
+		CallControlListener, SensorEventListener {
 
-	//static final String NUMBER_KEY = "number";
+	// static final String NUMBER_KEY = "number";
 	static final String CALL_INFO_KEY = "call_info";
-	static final long CLOSE_INTERVAL=3000;
-	static final int WHAT_CLOSE=0x001;
-	static final int WHAT_SERVICE_CONNECTED=0x002;
+	static final long CLOSE_INTERVAL = 3000;
+	static final int WHAT_CLOSE = 0x001;
+	static final int WHAT_SERVICE_CONNECTED = 0x002;
 
 	private ISip mSipService;
-	private TextView tvNameAndDtmf, tvState,tvDuration;
+	private TextView tvNameAndDtmf, tvState, tvDuration;
 	private FunctionsView vFunctions;
 	private CallControlView vCallControl;
 
@@ -54,32 +57,33 @@ public class CallActivity extends Activity implements FunctionsListener,
 	private int mDurationSeconds;
 	private Timer mDurationTimer;
 	private DurationTask mDurationTask;
-	
+	private SensorManager mSensorManager;// 传感器管理对象
+
 	@SuppressLint("HandlerLeak")
-	private Handler mHandler=new Handler(){
-		
+	private Handler mHandler = new Handler() {
+
 		@Override
 		public void handleMessage(android.os.Message msg) {
-			if(msg.what==WHAT_CLOSE){
+			if (msg.what == WHAT_CLOSE) {
 				CallActivity.this.finish();
-			}else if(msg.what==WHAT_SERVICE_CONNECTED){
-				if(mCallInfo.getState()==Constant.CallState.OUT_NOT_START){
+			} else if (msg.what == WHAT_SERVICE_CONNECTED) {
+				if (mCallInfo.getState() == Constant.CallState.OUT_NOT_START) {
 					makeCall();
 				}
 			}
 		}
 	};
-	
-	private ServiceConnection mSipServiceConn=new ServiceConnection() {
-		
+
+	private ServiceConnection mSipServiceConn = new ServiceConnection() {
+
 		@Override
 		public void onServiceDisconnected(ComponentName name) {
-			mSipService=null;
+			mSipService = null;
 		}
-		
+
 		@Override
 		public void onServiceConnected(ComponentName name, IBinder service) {
-			mSipService=ISip.Stub.asInterface(service);
+			mSipService = ISip.Stub.asInterface(service);
 			mHandler.sendEmptyMessage(WHAT_SERVICE_CONNECTED);
 		}
 	};
@@ -97,21 +101,35 @@ public class CallActivity extends Activity implements FunctionsListener,
 		this.vCallControl = (CallControlView) this
 				.findViewById(R.id.vCallControl);
 		this.vCallControl.setCallControlListener(this);
-		this.mCallStateReceiver=new CallStateReceiver();
-		this.registerReceiver(this.mCallStateReceiver, this.mCallStateReceiver.mFilter);
+		this.mCallStateReceiver = new CallStateReceiver();
+		this.registerReceiver(this.mCallStateReceiver,
+				this.mCallStateReceiver.mFilter);
 		WakeManaer.wakeUp(this);
+		this.mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		this.init(this.getIntent());
 	}
-	
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		this.mSensorManager.registerListener(this,
+				mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY),// 距离感应器
+				SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
 		this.unbindService(this.mSipServiceConn);
 		this.unregisterReceiver(this.mCallStateReceiver);
+		if (mSensorManager != null) {
+			// localWakeLock.release();//释放电源锁，如果不释放finish这个acitivity后仍然会有自动锁屏的效果，不信可以试一试
+			mSensorManager.unregisterListener(this);// 注销传感器监听
+		}
 	}
-	
-	private void bindSipService(){
-		Intent intent=new Intent();
+
+	private void bindSipService() {
+		Intent intent = new Intent();
 		intent.setAction(Constant.Action.SIP_SERVICE);
 		intent.setPackage("com.matteo.cc");
 		this.bindService(intent, this.mSipServiceConn, Service.BIND_AUTO_CREATE);
@@ -124,48 +142,52 @@ public class CallActivity extends Activity implements FunctionsListener,
 	}
 
 	private void init(Intent intent) {
-		this.mCallInfo=intent.getParcelableExtra(CALL_INFO_KEY);
-		String name = ContactUtils.getNameByNumber(this.mCallInfo.getNumber(), this);
+		this.mCallInfo = intent.getParcelableExtra(CALL_INFO_KEY);
+		String name = ContactUtils.getNameByNumber(this.mCallInfo.getNumber(),
+				this);
 		if (TextUtils.isEmpty(name)) {
 			this.mDisplayName = this.mCallInfo.getNumber();
 		} else {
 			this.mDisplayName = name;
 		}
 		this.vCallControl.updateCallState(this.mCallInfo);
-		this.mDtmf="";
+		this.mDtmf = "";
 		this.displayViews();
 	}
-	
-	private void makeCall(){
-		int result=Constant.Status.OK;
-		//this.tvNameAndDtmf.
+
+	private void makeCall() {
+		int result = Constant.Status.OK;
+		// this.tvNameAndDtmf.
 		try {
-			result=this.mSipService.makeCall(this.mCallInfo.getNumber());
+			result = this.mSipService.makeCall(this.mCallInfo.getNumber());
 		} catch (RemoteException e) {
 			e.printStackTrace();
-			result=Constant.Status.ERROR_DEFAULT;
+			result = Constant.Status.ERROR_DEFAULT;
 		}
-		if(result==Constant.Status.OK){
-			
-		}else{
-			Toast.makeText(this, this.getResources().getString(R.string.call_out_failed), Toast.LENGTH_SHORT).show();
+		if (result == Constant.Status.OK) {
+
+		} else {
+			Toast.makeText(this,
+					this.getResources().getString(R.string.call_out_failed),
+					Toast.LENGTH_SHORT).show();
 			this.closeActivity();
 		}
 	}
 
 	private void displayViews() {
 		this.tvState.setText(R.string.dialling);
-		if(TextUtils.isEmpty(this.mDtmf)){
+		if (TextUtils.isEmpty(this.mDtmf)) {
 			this.tvNameAndDtmf.setText(this.mDisplayName);
-		}else{
+		} else {
 			this.tvNameAndDtmf.setText(this.mDtmf);
 		}
 		this.updateCallInfo(this.mCallInfo);
 	}
-	
-	private void updateCallInfo(XCallInfo newCallInfo){
-		//this.mCallInfo=newCallInfo;
-		if(newCallInfo.getState()==this.mCallInfo.getState()&&newCallInfo.getDirect()==this.mCallInfo.getDirect()){
+
+	private void updateCallInfo(XCallInfo newCallInfo) {
+		// this.mCallInfo=newCallInfo;
+		if (newCallInfo.getState() == this.mCallInfo.getState()
+				&& newCallInfo.getDirect() == this.mCallInfo.getDirect()) {
 			return;
 		}
 		this.mCallInfo.setState(newCallInfo.getState());
@@ -200,15 +222,15 @@ public class CallActivity extends Activity implements FunctionsListener,
 		}
 		this.vCallControl.updateCallState(this.mCallInfo);
 	}
-	
-	private void refreshDuration(){
-		this.mDurationSeconds=0;
-		if(this.mDurationTimer!=null){
+
+	private void refreshDuration() {
+		this.mDurationSeconds = 0;
+		if (this.mDurationTimer != null) {
 			this.mDurationTimer.cancel();
 			this.mDurationTask.cancel();
 		}
-		this.mDurationTimer=new Timer();
-		this.mDurationTask=new DurationTask();
+		this.mDurationTimer = new Timer();
+		this.mDurationTask = new DurationTask();
 		this.mDurationTimer.schedule(this.mDurationTask, 0, 1000);
 	}
 
@@ -241,7 +263,7 @@ public class CallActivity extends Activity implements FunctionsListener,
 		this.vCallControl.showTvHideDtmfPad();
 	}
 
-	public static void startActivity(Context context,XCallInfo callInfo) {
+	public static void startActivity(Context context, XCallInfo callInfo) {
 		Intent intent = new Intent(context, CallActivity.class);
 		intent.putExtra(CALL_INFO_KEY, callInfo);
 		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -249,7 +271,8 @@ public class CallActivity extends Activity implements FunctionsListener,
 	}
 
 	public static void startActivity(Context context, String number) {
-		CallActivity.startActivity(context,  new XCallInfo(number, Constant.CallDirect.OUT, Constant.CallState.OUT_NOT_START));
+		CallActivity.startActivity(context, new XCallInfo(number,
+				Constant.CallDirect.OUT, Constant.CallState.OUT_NOT_START));
 	}
 
 	@Override
@@ -287,48 +310,71 @@ public class CallActivity extends Activity implements FunctionsListener,
 
 	@Override
 	public void onDtmfNumberClick(char dtmf) {
-		this.mDtmf=this.mDtmf+dtmf;
+		this.mDtmf = this.mDtmf + dtmf;
 		this.displayViews();
 	}
-	
-	private void closeActivity(){
+
+	private void closeActivity() {
 		this.mHandler.sendEmptyMessageDelayed(WHAT_CLOSE, CLOSE_INTERVAL);
 	}
-	
-	class CallStateReceiver extends BroadcastReceiver{
+
+	class CallStateReceiver extends BroadcastReceiver {
 
 		IntentFilter mFilter;
-		
-		CallStateReceiver(){
-			this.mFilter=new IntentFilter(Constant.Action.CALL_STATE_CHANGED);
+
+		CallStateReceiver() {
+			this.mFilter = new IntentFilter(Constant.Action.CALL_STATE_CHANGED);
 		}
-		
+
 		@Override
 		public void onReceive(Context context, Intent intent) {
-			XCallInfo newCallInfo=intent.getParcelableExtra(Constant.Key.CALL_INFO);
-			if(newCallInfo==null){
+			XCallInfo newCallInfo = intent
+					.getParcelableExtra(Constant.Key.CALL_INFO);
+			if (newCallInfo == null) {
 				return;
 			}
 			CallActivity.this.updateCallInfo(newCallInfo);
 		}
-		
+
 	}
-	
-	class DurationTask extends TimerTask{
+
+	class DurationTask extends TimerTask {
 
 		@Override
 		public void run() {
-			final int duration=mDurationSeconds++;
+			final int duration = mDurationSeconds++;
 			CallActivity.this.runOnUiThread(new Runnable() {
-				
+
 				@Override
 				public void run() {
-					int minutes=duration/60;
-					int seconds=duration%60;
-					tvDuration.setText(String.format("%02d:%02d", minutes,seconds));
+					int minutes = duration / 60;
+					int seconds = duration % 60;
+					tvDuration.setText(String.format("%02d:%02d", minutes,
+							seconds));
 				}
 			});
 		}
-		
+
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		float[] its = event.values;
+		// Log.d(TAG,"its array:"+its+"sensor type :"+event.sensor.getType()+" proximity type:"+Sensor.TYPE_PROXIMITY);
+		if (its != null && event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+			// 经过测试，当手贴近距离感应器的时候its[0]返回值为0.0，当手离开时返回1.0
+			if (its[0] == 0.0) {// 贴近手机
+				WakeManaer.lock(this);
+			} else {// 远离手机
+				WakeManaer.wakeUp(this);
+			}
+		}
+
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+
 	}
 }
